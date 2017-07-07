49514bc81d2f1199777badc73b3cc1225fb07273
Innovationbham_2 <Innovationbham_2@UABiLab-2s-iMac.local>
2017-07-07 14:25:00 -0500

    extra credit

--- scripts.js
+++ scripts.js
@@ -1,36 +1,38 @@
 $(document).ready(function(){
-    
 })
 
 dieArray=[]
 
-var Die=function(value){
-    this.value=value
-    var diceRoll=Math.floor(Math.random() * 6);
-    var div=document.createElement('div')
-    div.className='dice'
-    document.body.appendChild(div)
-    if(diceRoll>0){
-    div.innerHTML=diceRoll
-    }
-    else{
-        div.remove()
-    }
-    dieArray.push(diceRoll)
-
+var Die=function(){
+    var d = Math.floor(Math.random() * 6 + 1);
+    this.value = d;
+    this.div=document.createElement('div')
+    this.div.id='dice'
+    document.body.appendChild(this.div)
+    this.div.innerHTML = d;
 }
 
-//value to array
-var d=new Die(Math.floor(Math.random() * 6))
-
 Die.prototype.roll=function(){
-    console.log(this.value)
+    this.value=Math.floor(Math.random() * 6 + 1)
+    this.div.innerHTML=this.value;
 }
 
 $('#btn').on('click',function(){
-    Die()
+    var newDie=new Die()
+    dieArray.push(newDie)
     console.log(dieArray)
 })
 $('#roll').on('click',function(){
-    d.roll()
+    dieArray.forEach(function(element) {
+    element.roll()
+       });
+   //    console.log(dieArray)
+})
+/*$('#sum').on('click',function(){
+    dieArray.join('')
+ //   alert('sum here')
+})*/
+$('#dice').on('click',function(){
+    console.log('i was clicked')
+  //  this.div.roll()
 })
